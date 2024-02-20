9ee65fa8de4f80fac1819b55fe97a7eb0206ee4d
Mirela Mustic <mirela.mustic00@gmail.com>
2024-02-20 13:39:33 +0000

    added checkbox to my task list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,12 +31,12 @@ class TasksController < ApplicationController
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-    redirect_to task_path, status: :see_other
+    redirect_to tasks_path, status: :see_other
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :detail, :completed)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
